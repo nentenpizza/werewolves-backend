05ad46db977e5bff24ac55693630cb33d8972d2c
unknown <keked@ukr.net>
2021-01-12 19:36:43 +0300

    some more changes

--- cmd/game/game.go
+++ cmd/game/game.go
@@ -1,42 +1,44 @@
-package cmd
+package main
 
 import (
 	"encoding/json"
-	"github.com/nentenpizza/werewolves-backend/game"
 	"log"
 	"reflect"
 	"strconv"
+	"time"
+
+	"github.com/nentenpizza/werewolves/game"
 )
 
+//
 func main() {
 	players := game.Players{}
-	for i := 0; i < 2; i++ {
+	room := game.NewRoom("1", "sample_room", players, game.Settings{})
+	for i := 0; i < 10; i++ {
 		s := strconv.Itoa(i)
 		p := game.NewPlayer(s)
 		go func() {
-			select {
-			case <-p.Update:
-				b, err := json.Marshal(game.NewPlayerState(p))
-				if err != nil {
-					log.Println(err)
+			for {
+				select {
+				case <-p.Update:
+					b, err := json.Marshal(p)
+					if err != nil {
+						log.Println(err)
+					}
+					log.Println(string(b) + "\n----------------------------\n")
 				}
-				log.Println(string(b))
 			}
 		}()
 		players[s] = p
 	}
 
-	room := game.NewRoom(players, Settings{})
 	err := room.Run()
 	if err != nil {
 		panic(err)
 	}
-	constable := players["0"].Character.(*Constable)
-	err = room.Perform(constable.Shoot(players["1"]))
-	if err != nil {
-		panic(err)
-	}
+
 	for _, v := range players {
 		log.Printf("PlayerID: %s Role: %v", v.ID, reflect.TypeOf(v.Character))
 	}
+	time.Sleep(123 * time.Second)
 }
